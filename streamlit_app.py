0c440e839624adc004bfd081e0cb41cdc4d27ac1
venkataramanik <35788210+venkataramanik@users.noreply.github.com>
2025-08-11 14:35:57 -0400

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Venkat's SCM & Data Portfolio"
+)
+
+st.title("Venkat Krishnan")
+st.subheader("A playground for AI/ML solutions in Supply Chain Management")
+
+st.write("---")
+
+st.write("""
+I'm curious how AI and Machine Learning can solve real-world SCM problems at large and provide business value.
+
+This is a quick playground to explore those ideas. Navigate the sidebar to see projects that demonstrate how these concepts can be applied.
+""")
+
+st.write("---")
+
+st.subheader("Connect with Me")
+st.write("""
+- **LinkedIn:** [Venkat Krishnan](https://www.linkedin.com/in/venkrish1/)  
+
+""")
